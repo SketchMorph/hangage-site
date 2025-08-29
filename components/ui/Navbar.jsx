638d6f35a76c02fc72c7caec7af58861f694366a
SketchMorph <msp3692a@gmail.com>
2025-08-29 23:58:39 +0900

    Create Navbar.jsx

---
+++ components/ui/Navbar.jsx
@@ -0,0 +1,30 @@
+"use client"
+
+import Link from "next/link"
+
+export default function Navbar() {
+  return (
+    <nav className="flex items-center justify-between p-4 bg-white shadow">
+      {/* 로고 */}
+      <Link href="/" className="text-2xl font-bold">
+        한스타일 '밝은'
+      </Link>
+
+      {/* 메뉴 */}
+      <div className="flex space-x-4">
+        <Link href="/products" className="hover:text-blue-600">
+          상품목록
+        </Link>
+        <Link href="/login" className="hover:text-blue-600">
+          로그인
+        </Link>
+        <Link href="/signup" className="hover:text-blue-600">
+          회원가입
+        </Link>
+        <Link href="/admin" className="bg-blue-500 text-white px-3 py-1 rounded hover:bg-blue-600">
+          상품 등록
+        </Link>
+      </div>
+    </nav>
+  )
+}
