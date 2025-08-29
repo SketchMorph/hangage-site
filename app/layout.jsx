08bfb5b3af8fc3a17aaea3ba95b6a9502845f84f
SketchMorph <msp3692a@gmail.com>
2025-08-30 00:00:12 +0900

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,18 @@
-import '../styles/globals.css';
+import "./globals.css"
+import Navbar from "@/components/Navbar"
 
 export const metadata = {
-  title: "한스타일 '밝은' 상점",
-  description: "우리옷 상점, 한스타일 '밝은'상점",
-};
+  title: "한스타일 밝은",
+  description: "생활한복 상점 사이트",
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body className="min-h-screen flex flex-col">
+        <Navbar />
+        <main className="flex-1">{children}</main>
+      </body>
     </html>
-  );
+  )
 }
