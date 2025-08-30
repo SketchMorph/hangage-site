0d4d2c824ce15c7e6cdea8b0ad5c314237d5e38d
SketchMorph <msp3692a@gmail.com>
2025-08-30 16:22:20 +0900

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,18 +1,22 @@
-import "../styles/globals.css"
-import Navbar from "@/components/ui/Navbar"
+import "../styles/globals.css";
+import Navbar from "@/components/ui/Navbar";
+import { usePathname } from "next/navigation";
 
 export const metadata = {
-  title: "한스타일 밝은",
+  title: "한스타일",
   description: "생활한복 상점 사이트",
-}
+};
 
 export default function RootLayout({ children }) {
+  const pathname = usePathname();
+  const currentLang = pathname.split("/")[1] || "ko"; // URL 첫 부분 (/ko, /en...)
+
   return (
-    <html lang="ko">
+    <html lang={currentLang}>
       <body className="min-h-screen flex flex-col">
         <Navbar />
         <main className="flex-1">{children}</main>
       </body>
     </html>
-  )
+  );
 }
