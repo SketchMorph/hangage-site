4b4b4f7e11c040eba14b2230444b566c51e4270b
SketchMorph <msp3692a@gmail.com>
2025-09-01 18:57:28 +0900

    Delete app/en directory

--- app/en/login/page.jsx
+++
@@ -1,6 +0,0 @@
-import LoginForm from "@/components/pages/LoginForm";
-import dict from "@/locales/en.json";
-
-export default function Page() {
-  return <LoginForm dict={dict} />;
-}
--- app/en/products/page.jsx
+++
@@ -1,6 +0,0 @@
-import ProductList from "@/components/pages/ProductList";
-import dict from "@/locales/en.json";
-
-export default function Page() {
-  return <ProductList dict={dict} />;
-}
