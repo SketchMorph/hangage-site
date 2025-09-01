0a237e0ad06e299e9bc31aadff35f0a26156fb3a
SketchMorph <msp3692a@gmail.com>
2025-09-01 18:50:06 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true };
+const nextConfig = {
+  i18n: {
+    locales: ["ko", "en", "fr", "ja", "zh"], // 지원 언어
+    defaultLocale: "ko",                     // 기본 언어
+  },
+};
+
 module.exports = nextConfig;
