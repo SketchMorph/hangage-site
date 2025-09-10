a1d8948012ec176425d0fcd06518bd832d9095e1
SketchMorph <msp3692a@gmail.com>
2025-09-10 19:56:15 +0900

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,6 @@
 import HanbokLanding from "./_landing/HanbokLanding";
 
 export default function HomePage() {
-  // / 는 한국어 랜딩 기본값
+  // 루트(/)에서는 한국어 랜딩 기본값
   return <HanbokLanding lang="ko" />;
 }
